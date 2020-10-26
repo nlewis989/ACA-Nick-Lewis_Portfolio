3515f0fd1aa16c77a48ed06fc7f048ad3eb26b52
NLEWIS989 <LONGESTYARD43@GMAIL.COM>
2020-10-26 16:06:28 -0500

    aster

---
+++ resume-interactive.js
@@ -0,0 +1,11 @@
+function fadenamein() {
+    const element = document.getElementById("name");
+    element.classList.add("animate__animated", ".animate__fadeInDown");
+}
+
+if (window.function_name) {
+    // script loaded
+} else {
+    // script not loaded
+}
+
