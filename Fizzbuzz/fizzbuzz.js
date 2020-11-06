575cd956133ab725f34ddfa70d51a9acf25cc173
nlewis989 <71626506+nlewis989@users.noreply.github.com>
2020-11-05 18:05:25 -0600

    Add files via upload

---
+++ Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,17 @@
+console.log('Hello fizzbuzz')
+
+
+let i = 0 
+let thousand = 1000
+
+while (i < 1000) {
+
+    if (i % 15 == 0) console.log("Fizz Buzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+}   i++
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}   
